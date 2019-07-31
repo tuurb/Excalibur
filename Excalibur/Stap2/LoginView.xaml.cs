af051ca9116c6eae5803b3da7ae943cb53b638fe
Arthur Brugts <arthur.brugts@greenchoice.nl>
2019-07-31 18:07:08 +0200

    Stap 2 aanpassingen

--- Excalibur/Stap2/LoginView.xaml.cs
+++ Excalibur/Stap2/LoginView.xaml.cs
@@ -1,76 +1,20 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-
-namespace Excalibur.Stap2
+﻿namespace Excalibur.Stap2
 {
-    public partial class LoginView : INotifyPropertyChanged
+    public partial class LoginView
     {
-        private string _username;
-        private string _password;
-        private string _status;
-        private int _selectedSleepTime;
-
-        public string Username
-        {
-            get => _username;
-            set
-            {
-                _username = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public string Password
-        {
-            get => _password;
-            set
-            {
-                _password = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public string Status
-        {
-            get => _status;
-            set
-            {
-                _status = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public List<int> AvailableSleepTimes { get; }
-
-        public int SelectedSleepTime
-        {
-            get => _selectedSleepTime;
-            set
-            {
-                _selectedSleepTime = value;
-                OnPropertyChanged();
-            }
-        }
-
+        public LoginViewModel ViewModel => (LoginViewModel)DataContext;
 
         public LoginView()
         {
             InitializeComponent();
-            DataContext = this;
-
-            AvailableSleepTimes = new List<int>(Enumerable.Range(0, 10));
-            SelectedSleepTime = 2;
+            DataContext = new LoginViewModel();
         }
 
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        private void LoginButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            ViewModel.Status = ViewModel.IsLoginGeslaagd 
+                ? $"{ViewModel.Username} is succesvol ingelogd" 
+                : $"Inloggen van gebruiker {ViewModel.Username} is niet geslaagd";
         }
     }
-
 }
--- Excalibur/Stap2/LoginViewModel.cs
+++ Excalibur/Stap2/LoginViewModel.cs
@@ -1,6 +1,82 @@
-﻿namespace Excalibur.Stap2
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Excalibur.Stap2
 {
     public class LoginViewModel : BindableBase
     {
+        private string _username;
+        private string _password;
+        private string _status;
+        private int _selectedSleepTime;
+        private bool _loginButtonIsEnabled;
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                if (SetIfChanged(ref _username, value))
+                {
+                    SetLoginButtonIsEnabled();
+                }
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (SetIfChanged(ref _password, value))
+                {
+                    SetLoginButtonIsEnabled();
+                }
+            }
+        }
+
+        public string Status
+        {
+            get => _status;
+            set => SetIfChanged(ref _status, value);
+        }
+
+        public int SelectedSleepTime
+        {
+            get => _selectedSleepTime;
+            set => SetIfChanged(ref _selectedSleepTime, value);
+        }
+
+        public bool LoginButtonIsEnabled
+        {
+            get => _loginButtonIsEnabled;
+            set
+            {
+                if (SetIfChanged(ref _loginButtonIsEnabled, value))
+                {
+                    if (!_loginButtonIsEnabled)
+                    {
+                        Status = null;
+                    }
+                }
+            }
+        }
+
+        public List<int> AvailableSleepTimes { get; }
+
+        public bool IsLoginGeslaagd => Username == Password;
+
+        public LoginViewModel()
+        {
+            AvailableSleepTimes = new List<int>(Enumerable.Range(0, 10));
+            SelectedSleepTime = 2;
+
+            LoginButtonIsEnabled = false;
+        }
+
+        private void SetLoginButtonIsEnabled()
+        {
+            LoginButtonIsEnabled = !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password);
+        }
     }
 }
